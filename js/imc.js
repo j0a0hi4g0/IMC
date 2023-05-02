a28c6670a70fdd3a8413e0d0edf02119f7f886dc
hiago <joao.hiago.c.barros.com>
2023-05-02 19:56:48 -0300

    commit 9

--- js/imc.js
+++ js/imc.js
@@ -1,7 +1,22 @@
-function calcularIMC() {
+function selecionarCalculadora() {
+    var idade = document.getElementById("idade").value;
+    if (idade == "idoso") {
+      document.getElementById("peso").value = "";
+      document.getElementById("altura").value = "";
+      document.getElementById("resultado").innerHTML = "";
+    }
+  }
+  
+  function calcularIMC() {
+    var idade = document.getElementById("idade").value;
     var peso = parseFloat(document.getElementById("peso").value);
     var altura = parseFloat(document.getElementById("altura").value);
-    var imc = peso / (altura * altura);
+    var imc = 0;
+    if (idade == "adulto") {
+      imc = peso / (altura * altura);
+    } else if (idade == "idoso") {
+      imc = 1.3 * peso / (altura * altura);
+    }
     document.getElementById("resultado").innerHTML = "Seu IMC é " + imc.toFixed(2);
   }
   
